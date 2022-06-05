4111ce3d75c60e875882196dd0dc0114bcb7b8a6
sonukumarraj007 <sonukumarraj007@gmail.com>
2022-06-05 10:27:50 +0530

    directive-code-added

--- src/app/example/directives/directives.component.ts
+++ src/app/example/directives/directives.component.ts
@@ -9,7 +9,25 @@ export class DirectivesComponent implements OnInit {
 
   constructor() { }
 
+  inputPass:number = 10
+  inputPass2: number = 5;
+  inputPass3: number = -1;
+
+  language_list = [
+    'C', 'C++', 'Java', 'Python', 'CSS'
+  ]
+
   ngOnInit(): void {
+  }
+
+  getColor(number: number){
+  if(number > 0){
+    return 'green'
+  }if(number <0){
+    return 'red'
+  }else{
+    return '#fff'
+  }
   }
 
 }
